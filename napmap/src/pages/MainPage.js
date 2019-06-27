b03344d1bb099318bae8838f7fbb04ad6dd305b9
elsa <sakohhh@outlook.com>
2019-06-27 12:46:53 +0800

    pages

---
+++ napmap/src/pages/MainPage.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+class MainPage extends Component {
+    render(){
+        return(
+            <View style={styles.container}>
+                <TouchableOpacity onPress={() => Actions.JourneyPage()}>
+                    <Text>Main </Text>
+                </TouchableOpacity>
+            </View>
+           
+        );
+    }
+}
+export default MainPage;
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+  });
+  
