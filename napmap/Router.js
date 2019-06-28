d11a3b54e187731bdf43a626c7d8f5e035bb58e3
elsa <sakohhh@outlook.com>
2019-06-28 16:06:03 +0800

    profilepage

--- napmap/Router.js
+++ napmap/Router.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { Actions, Scene, Router, Modal, Tabs } from 'react-native-router-flux';
 import MainPage from './src/pages/MainPage';
 import JourneyPage from './src/pages/JourneyPage';
+import ProfilePage from './src/pages/ProfilePage';
+
 class RouterComponent extends Component {
     render(){
         return(
             <Router gesturesEnabled={false}>
                 <Scene key="root" hideNavBar>
-                    <Scene initial key="MainPage" component={MainPage} hideNavBar/>
-                    <Scene key="JourneyPage" component={JourneyPage} hideNavBar/>
+                    <Scene initial key="MainPage" component={MainPage} hideNavBar />
+                    <Scene key="JourneyPage" component={JourneyPage} hideNavBar />
+                    <Scene key="ProfilePage" component={ProfilePage} hideNavBar />
+
                 </Scene>
             </Router>
         );
