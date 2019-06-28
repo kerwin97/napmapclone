ef5ee4092645b7bb4da70f7a515e6b23c2a8ec1a
Kerwin Lim <kerwinlim@Kerwins-MacBook-Pro.local>
2019-06-28 18:08:18 +0800

    installed maps and firebase with cocoapods

--- napmap/android/app/src/main/java/com/napmap/MainApplication.java
+++ napmap/android/app/src/main/java/com/napmap/MainApplication.java
@@ -3,6 +3,7 @@ package com.napmap;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
 import com.transistorsoft.rnbackgroundgeolocation.RNBackgroundGeolocation;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new MapsPackage(),
             new RNBackgroundFetchPackage(),
             new RNBackgroundGeolocation()
--- napmap/ios/napmap/AppDelegate.m
+++ napmap/ios/napmap/AppDelegate.m
@@ -4,17 +4,19 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
+//removed libAirMaps.a from the napmap build phase link lib
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyBNkgiBLRx5GUh8yBnfAdT82Lhp6eF1j3Y"];  // add this line using the api key obtained from Google Console
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"napmap"
@@ -27,8 +29,11 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  //[GMSPlacesClient.provideAPIKey:@"AIzaSyBNkgiBLRx5GUh8yBnfAdT82Lhp6eF1j3Y"];
+  //[FIRApp configure];
   return YES;
 }
+
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
