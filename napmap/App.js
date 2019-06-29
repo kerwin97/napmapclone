697b97e66b8c62c4d97c55d55a9f73210709c054
elsa <sakohhh@outlook.com>
2019-06-29 13:12:53 +0800

    some extra changes, cleaned some code

--- napmap/App.js
+++ napmap/App.js
@@ -1,11 +1,13 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
 import Router from './Router';
 
+//for testing!! 
+console.disableYellowBox = true;
 class App extends Component {
   render() {
     return (
-      <View style={{flex:1}}>
+      <View style={{ flex: 1 }}>
         <Router />
       </View>
     );
--- napmap/Router.js
+++ napmap/Router.js
@@ -3,15 +3,19 @@ import { Actions, Scene, Router, Modal, Tabs } from 'react-native-router-flux';
 import MainPage from './src/pages/MainPage';
 import JourneyPage from './src/pages/JourneyPage';
 import ProfilePage from './src/pages/ProfilePage';
+import AddRoutePage from './src/pages/AddRoutePage';
+import MapPage from './src/pages/MapPage';
 
 class RouterComponent extends Component {
-    render(){
-        return(
+    render() {
+        return (
             <Router gesturesEnabled={false}>
                 <Scene key="root" hideNavBar>
                     <Scene initial key="MainPage" component={MainPage} hideNavBar />
                     <Scene key="JourneyPage" component={JourneyPage} hideNavBar />
                     <Scene key="ProfilePage" component={ProfilePage} hideNavBar />
+                    <Scene key="AddRoutePage" component={AddRoutePage} hideNavBar /> 
+                    <Scene key="MapPage" component={MapPage} hideNavBar />                   
 
                 </Scene>
             </Router>
